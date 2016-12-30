ef47288ba39dfaa49a871a90d965ede88e99294d
Ole Lynge Soerensen <olelynge@gmail.com>
2016-12-30 07:05:37 +0100

    Cache words.

--- games.spelling/WordService.m
+++ games.spelling/WordService.m
@@ -13,16 +13,36 @@
 
 - (void)FetchWords:(void (^)(NSMutableArray<Word *> *))handler {
     _completionHandler = [handler copy];
+
+    NSString *cacheFolder = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [cacheFolder stringByAppendingPathComponent:@"SpellingGame-Words.txt"];
+
+    // TODO: Check whether words are already in cache
+    BOOL isDirectory = NO;
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath isDirectory:&isDirectory]) {
+        NSDictionary *cachedResult = [NSDictionary dictionaryWithContentsOfFile:filePath];
+        [self returnResult:cachedResult];
+    }
+
     RestService *restService = [[RestService alloc] init];
     NSString *apiCode = @"l7YSboIW6rgQBaavhoF24p6gvHApLaTt2/OX4urNlWYisINNkqsRtQ==";
     NSString *game = @"spelling";
     NSString *urlString = [NSString stringWithFormat:
             @"https://uncas.azurewebsites.net/api/HttpTriggerJS1?code=%@&game=%@", apiCode, game];
     [restService DownloadJson:urlString :^(NSDictionary *result) {
-        NSMutableArray<Word *> *words = [self parseWords:result];
-        _completionHandler(words);
-        _completionHandler = nil;
+        // TODO: Add words to cache
+        [result writeToFile:filePath atomically:YES];
+        [self returnResult:result];
     }];
+}
+
+- (void)returnResult:(NSDictionary *)result {
+    if (!_completionHandler)
+        return;
+
+    NSMutableArray<Word *> *words = [self parseWords:result];
+    _completionHandler(words);
+    _completionHandler = nil;
 }
 
 - (NSMutableArray<Word *> *)parseWords:(NSDictionary *)result {
